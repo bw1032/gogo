3b62a8637bcf0eafb5d1b009474ed4ef365bdda3
Your Name <you@example.com>
2021-12-17 16:59:28 +0800

    m

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 
 1. go test -v 显示详细信息
 2. go test -v a.go b.go -test.run TestA 执行具体方法
-3. go test -run TestC  执行具体方法 加载所有.go 文件
+3. go test -run TestC  执行具体方法 加载所有.go 文件 
+4. go test benchmem
 
 ##### 面试
 1. [值传递和引用传递](https://github.com/bw1032/gogo/blob/main/interview/a.go) 
-2. [字符串下标取值](https://github.com/bw1032/gogo/blob/main/interview/b.go) 
+2. [中文字符串下标取值](https://github.com/bw1032/gogo/blob/main/interview/b.go) 
 3. [字符串转成byte数组，会发生内存拷贝吗？](https://www.jianshu.com/p/e45f2a69f0aa)
 
 ##### 常用
--- interview/b.go
+++ interview/b.go
@@ -6,9 +6,6 @@ import (
 	"golang.org/x/exp/utf8string"
 )
 
-//string和byte区别 (&go test 基准测试 benchmem): https://www.jianshu.com/p/e45f2a69f0aa
-
-
 func B() {
 
 	//string的索引操作返回的是byte（或uint8），
@@ -16,19 +13,14 @@ func B() {
 	//byte 等同于int8，常用来处理ascii字符
 	//rune 等同于int32,常用来处理unicode或utf-8字符
 	var c = "a你好"
-	fmt.Println(c[0]) //97
-	fmt.Println(c[1]) //228
-	fmt.Println(string(c[1])) //ä
-	fmt.Println(utf8string.NewString(c).At(0)) //97
-	fmt.Println(utf8string.NewString(c).At(1)) //20320
+	fmt.Println(c[0], c[1], string(c[1])) //97 //228 //ä
+	fmt.Println(utf8string.NewString(c).At(0), utf8string.NewString(c).At(1)) //97 20320
 	fmt.Println(string(utf8string.NewString(c).At(1))) //你
 	fmt.Println(utf8string.NewString(c).Slice(1,2))  //你
 
 	//utf8.RuneCountInString(a) 字符长度  len(a) 字节长度
 	var a = "a"
-	fmt.Println(utf8.RuneCountInString(a)) //1
-	fmt.Println(len(a)) //1
+	fmt.Println(utf8.RuneCountInString(a), len(a)) //1 //1
 	var b = "你好"
-	fmt.Println(utf8.RuneCountInString(b)) //2
-	fmt.Println(len(b)) //6
+	fmt.Println(utf8.RuneCountInString(b), len(b)) //2 //6
 }
